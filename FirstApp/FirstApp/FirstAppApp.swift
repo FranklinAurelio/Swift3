67825ff6045d1b96f912c709aaf2e68afe8800e0
FranklinAurelio <52283001+FranklinAurelio@users.noreply.github.com>
2021-02-19 11:22:27 -0300

    Add files via upload

---
+++ FirstApp/FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Franklin Aurelio on 19/02/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
